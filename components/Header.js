87b609ca0a611483f280cf660558be8f870b9a6a
Jimmy Lu <jimmy.lu.2011@gmail.com>
2021-03-27 21:20:31 -0700

    second commit

--- components/Header.js
+++ components/Header.js
@@ -9,6 +9,19 @@ const Header = (props) => {
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  header: {
+    width: '100%',
+    height: 90,
+    paddingTop: 36,
+    backgroundColor: '#f7287b',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  headerTitle: {
+    color: 'black',
+    fontSize: 18
+  }
+})
 
 export default Header
